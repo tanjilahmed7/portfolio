21bd3ab4583d761f76c221984cf448b1630601b1
root <root@vmi3082883.contaboserver.net>
2026-07-04 21:26:19 +0200

    Add Google Tag Manager (GTM-NL5BWZFG)

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,6 +69,23 @@ export default function RootLayout({
       className={`${inter.variable} ${spaceGrotesk.variable} ${jetbrainsMono.variable} dark antialiased`}
     >
       <body className="font-sans">
+        <Script id="gtm-init" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NL5BWZFG');
+          `}
+        </Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NL5BWZFG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-Z7CQPL5ZD3"
           strategy="afterInteractive"
